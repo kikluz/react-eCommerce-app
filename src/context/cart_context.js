449c0edefbaa771092a99039ed0530e7f57c1427
kikluz <savir81@hotmail.com>
2022-04-30 10:32:04 -0700

    add state and reducer of cart

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,13 +8,22 @@ import {
   COUNT_CART_TOTALS,
 } from '../actions'
 
-const initialState = {}
+
+const initialState = {
+  cart:[],
+  total_items:0,
+  total_amount: 0,
+  shipping_feed: 534,
+}
 
 const CartContext = React.createContext()
 
 export const CartProvider = ({ children }) => {
+  // setup ueseReducer and pass the reducer thats is comming from the cart_reducer
+  // and pass the initialState 
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <CartContext.Provider value='cart context'>{children}</CartContext.Provider>
+    <CartContext.Provider value={{...state }}>{children}</CartContext.Provider>
   )
 }
 // make sure use
